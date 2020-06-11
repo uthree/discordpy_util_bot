d844509d3c1971afa9bff8578fd738061a0061ab
uuu0614 <33905956+uuu0614@users.noreply.github.com>
2020-06-11 11:33:10 +0900

    initial commit

--- main.py
+++ main.py
@@ -9,3 +9,16 @@ if __name__ == "__main__":
     if not os.path.exists("token.yml"):
         with open("token.yml", "w") as file:
             yaml.dump(default_token_file, file)
+            print(
+                """
+                token.ymlファイルを編集して、botトークンを追加してください。
+                デフォルトでは main: 項目に追加することでbotが使用可能になります。
+                """
+            )
+            exit()
+    else:
+        with open("token.yml") as file:
+            token_data = yaml.safe_load(file)
+        using_token = token_data['using']
+        token = token_data[using_token]
+        print(f"{using_token} のトークンを使用します。")
