73af8cd33c680d3bd824c9d07390d1f3572a1755
uuu0614 <33905956+uuu0614@users.noreply.github.com>
2020-06-11 11:59:13 +0900

    とりあえずトークン洗

---
+++ general/testcog.py
@@ -0,0 +1,21 @@
+from discord.ext import commands  # Bot Commands Frameworkのインポート
+
+# コグとして用いるクラスを定義。
+
+
+class TestCog(commands.Cog):
+
+    # TestCogクラスのコンストラクタ。Botを受取り、インスタンス変数として保持。
+    def __init__(self, bot):
+        self.bot = bot
+
+    # コマンドの作成。コマンドはcommandデコレータで必ず修飾する。
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send('pong!')
+
+# Bot本体側からコグを読み込む際に呼び出される関数。
+
+
+def setup(bot):
+    bot.add_cog(TestCog(bot))  # TestCogにBotを渡してインスタンス化し、Botにコグとして登録する。
--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 import os
 import yaml
 
+from discord.ext import commands
+
+
+class UtilBot(commands.Bot):
+    # コンストラクタ
+    def __init__(self, command_prefix):
+        # スーパークラスのコンストラクタに値を渡して実行。
+        super().__init__(command_prefix)
+
+        # INITIAL_COGSに格納されている名前から、コグを読み込む。
+        # エラーが発生した場合は、エラー内容を表示。
+        for cog in INITIAL_EXTENSIONS:
+            try:
+                self.load_extension(cog)
+            except Exception:
+                traceback.print_exc()
+
+    # Botの準備完了時に呼び出されるイベント
+    async def on_ready(self):
+        print('-----')
+        print(self.user.name)
+        print(self.user.id)
+        print('-----')
+
+
 default_token_file = {
     'using': 'main',
     'main': '<YOUR BOT TOKEN HERE>'
@@ -17,8 +42,16 @@ if __name__ == "__main__":
             )
             exit()
     else:
+        # トークン読み込み処理
         with open("token.yml") as file:
             token_data = yaml.safe_load(file)
         using_token = token_data['using']
         token = token_data[using_token]
         print(f"{using_token} のトークンを使用します。")
+
+        # cog読み込み処理
+        with open("load_cogs.yml") as file:
+            INITIAL_EXTENSIONS = yaml.safe_load(file)['cogs']
+        # UtilBotのインスタンス化及び起動処理。
+        bot = UtilBot(command_prefix='!')
+        bot.run(token)  # Botのトークン
