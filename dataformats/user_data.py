bb0117cc13c51bc8fec0e66438a436cd811617f8
uuu0614 <33905956+uuu0614@users.noreply.github.com>
2020-09-07 20:28:41 +0900

    データ類をdataclassに変更

--- dataformats/user_data.py
+++ dataformats/user_data.py
@@ -1,11 +1,3 @@
+@dataclass
 class UserData:
-    def __init__(self):
-        self._profile = "プロフィール未設定"
-
-    @property
-    def profile():
-        return self_profile
-
-    @profile.setter
-    def set_profile(prof):
-        self._profile = prof
+    profile: str = "未設定"
