80693cca97b478d6ad4ebb23bf5d1a4e5aed1b19
uuu0614 <33905956+uuu0614@users.noreply.github.com>
2020-10-16 22:55:25 +0900

    スレッド用のデータだけ用意

--- dataformats/channel_data.py
+++ dataformats/channel_data.py
@@ -4,4 +4,8 @@ from dataclasses import dataclass
 @dataclass
 class ChannelData:
     channel_command: str = "",
+
+    thread_creater: bool = False,  # スレッド作成用チャンネルかどうか
+    thread: bool = False,  # これ自身がスレッドであるかどうか。
+    # config系
     adblock: bool = False,
