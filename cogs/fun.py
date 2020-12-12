e74eb78953b19de9e18aca7eee6bd238cd039b4e
uuu0614 <33905956+uuu0614@users.noreply.github.com>
2020-12-13 05:20:49 +0900

    added sinchoku command.

--- cogs/fun.py
+++ cogs/fun.py
@@ -15,7 +15,12 @@ class Fun(commands.Cog):
     @commands.command(aliases=["sintyoku"])
     async def sinchoku(self,ctx):
         links = [
-            "https://cdn.discordapp.com/attachments/708828790796845189/787409537883439134/alicedoudesuka-300x168.jpg"
+            "https://cdn.discordapp.com/attachments/708828790796845189/787409537883439134/alicedoudesuka-300x168.jpg",
+            "https://cdn.discordapp.com/attachments/787413181277798400/787413202958417920/images-6.jpeg",
+            "https://cdn.discordapp.com/attachments/787413181277798400/787413219085647872/images-5.jpeg",
+            "https://cdn.discordapp.com/attachments/787413181277798400/787413268872429568/images.jpeg",
+            "https://cdn.discordapp.com/attachments/787413181277798400/787413301776089118/main.pngcompresstrue.png",
+            "https://cdn.discordapp.com/attachments/787413181277798400/787413247686737930/images-3.jpeg"
         ]
         await ctx.send(random.choice(links))
     
