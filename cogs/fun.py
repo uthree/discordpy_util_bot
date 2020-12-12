32692bc3a06c7f62f518bac1adb7cdc3167f2e42
uuu0614 <33905956+uuu0614@users.noreply.github.com>
2020-12-13 05:12:54 +0900

    added sinchoku command.

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,5 +1,6 @@
 from discord.ext import commands  # Bot Commands Frameworkのインポート
 import discord as discord
+import random
 # ネタ系コマンド cog
 
 # コグとして用いるクラスを定義。
@@ -10,6 +11,13 @@ class Fun(commands.Cog):
     # Funクラスのコンストラクタ。Botを受取り、インスタンス変数として保持。
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command(aliases=["sintyoku"])
+    def sintyoku(self,ctx):
+        links = [
+            "https://cdn.discordapp.com/attachments/708828790796845189/787409537883439134/alicedoudesuka-300x168.jpg"
+        ]
+        ctx.send(random.choice(links))
     
 
 
